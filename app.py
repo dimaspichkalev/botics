eca2cb22d0f3d6562321053a8719d76226a24a7e
dimasextasy <dimaspichkalev@gmail.com>
2019-11-11 22:34:28 +0500

    update

--- app.py
+++ app.py
@@ -24,8 +24,8 @@ def hello():
 
 @app.before_request
 def before_request():
-if request.url.startswith('http://'):
-url = request.url.replace('http://', 'https://', 1)
+	if request.url.startswith('http://'):
+		url = request.url.replace('http://', 'https://', 1)
 
 
 @app.route('/', methods = ['POST'])
