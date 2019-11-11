c25f2bc70a42b6ec224558f76e188fb8aaa636f1
dimasextasy <dimaspichkalev@gmail.com>
2019-11-11 22:30:51 +0500

    update

--- app.py
+++ app.py
@@ -20,6 +20,14 @@ app = Flask(__name__)
 def hello():
 	return "hello"
 
+
+
+@app.before_request
+def before_request():
+if request.url.startswith('http://'):
+url = request.url.replace('http://', 'https://', 1)
+
+
 @app.route('/', methods = ['POST'])
 
 def message():
