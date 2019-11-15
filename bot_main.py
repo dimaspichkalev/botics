1e2f21fe20a0c64ea5f1da5a220b16d369d53f07
Olya <fedyaeva95@mail.ru>
2019-11-15 18:57:32 +0500

    откат

--- bot_main.py
+++ bot_main.py
@@ -14,21 +14,17 @@ extractors = {'open_card': TaskExtractor(prepare_dataset())}
 def analyze_message(message_text):
 	code_response = extractor.extract_tasks(message_text)
 	response = 'Ты мне написал: {0}, из этого я выделил следущее: {1}\n\n'.format(message_text, code_response)
-	return str(code_response)
-	# if code_response != '':
-	# 	if code_response in extractors:
-	# 		inside_extractor = extractors[code_response]
-	# 		if code_response == 'open_card':
-	# 			full_url = get_open_card_user_url(inside_extractor, message_text)
-	# 			return response + full_url
-	# 		if code_response == 'open_tasks':
-	# 			#full_url = get_open_card_user_url(inside_extractor, message_text)
-	# 			response = "мне нужно открыть ваши задачи"
-	# 			return response
-	# 	else:
-	# 		return 'Я увидел команду {0}, но не знаю что с ней делать :('.format(code_response)
-	# else:
-	# 	return 'Ничего не понял'
+	
+	if code_response != '':
+		if code_response in extractors:
+			inside_extractor = extractors[code_response]
+			if code_response == 'open_card':
+				full_url = get_open_card_user_url(inside_extractor, message_text)
+				return response + full_url
+		else:
+			return 'Я увидел команду {0}, но не знаю что с ней делать :('.format(code_response)
+	else:
+		return 'Ничего не понял'
 	
 
 def get_open_card_user_url(inside_extractor, message_text):
@@ -36,6 +32,3 @@ def get_open_card_user_url(inside_extractor, message_text):
 	url_base = 'https://dev.greendatasoft.ru/#/card/'
 	full_url = url_base + str(client_id)
 	return full_url
-
-def get_open_user_tasks_url():
-	url = 1
