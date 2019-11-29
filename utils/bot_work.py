0e28a4a7a856a91678069402693b78cea09224c3
Olya <fedyaeva95@mail.ru>
2019-11-30 01:20:03 +0500

    вернуть название орг тест

--- utils/bot_work.py
+++ utils/bot_work.py
@@ -8,25 +8,29 @@ from tasks.open_card import prepare_dataset, get_open_card_task_response
 # data = data.set_index('task').T.to_dict('list')
 # normalized_tasks_set = preprocess_tasks_set(data)
 # main_extractor = TaskExtractor(normalized_tasks_set)
+url_my_tasks = 'https://dev.greendatasoft.ru/#/registry/Task/881611'
 main_extractor = get_all_bot_commands()
-extractors = {'open_card': TaskExtractor(prepare_dataset()), 'get_tasks': 'https://dev.greendatasoft.ru/#/registry/Task/'}
+extractors = {'open_card': TaskExtractor(prepare_dataset()), 'get_tasks': url_my_tasks}
 
 
 def analyze_message(message_text):
 	code_response = main_extractor.extract_tasks(message_text)
-	response = 'Ты мне написал: {0}, из этого я выделил следущее: {1}\n\n'.format(message_text, code_response)
+	response = 'Ты мне написал: {0}, из этого я выделил следущее: '.format(message_text)
 	
 	if code_response != '':
 		if code_response in extractors:
 			inside_extractor = extractors[code_response]
+			return inside_extractor
 			if code_response == 'open_card':
+				response += 'открыть карточку'
 				task_response = get_open_card_task_response(inside_extractor, message_text)
 				return response + task_response
 			if code_response == 'get_tasks':
-				task_response = inside_extractor
-				return response + task_response
+				response += 'открыть задачи'
+				task_response = response + inside_extractor
+				return task_response
 		else:
-			return 'Я увидел команду {0}, но не знаю что с ней делать :( '.format(code_response)
+			return 'Я увидел команду {0}, но не знаю что с ней делать :('.format(code_response)
 	else:
-		return 'Ничего не понял, но все меняется'
+		return 'Я не знаю такой команды'
 
