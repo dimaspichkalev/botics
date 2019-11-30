be9448ebb094f686b555cd22ac18f7c38ed92dd9
dimasextasy <dimaspichkalev@gmail.com>
2019-11-30 08:03:11 +0500

    новый датасет

--- tasks/open_card.py
+++ tasks/open_card.py
@@ -21,11 +21,11 @@ def prepare_dataset():
 	# data = pd.read_csv('urici.csv', sep=';')
 	# data = data.set_index('task').T.to_dict('list')
 	normalized_clients_set = preprocess_tasks_set(clients_dic)
-	return normalized_clients_set
+	return classic_set, normalized_clients_set
 
 
 def get_open_card_task_response(message_text):
-	ur_dataset = prepare_dataset()
+	classis_set, ur_dataset = prepare_dataset()
 	inside_extractor = TaskExtractor(ur_dataset)
 	client_id = inside_extractor.extract_tasks(message_text)
 	client_list = client_id.split(' ')
@@ -34,7 +34,7 @@ def get_open_card_task_response(message_text):
 		response = "По данному запросу найдены следующие организации: \n\n"
 		url_card = "https://dev.greendatasoft.ru/#/card"
 		for cid in client_list:
-			client_name = ur_dataset[int(cid)]
+			client_name = classis_set[int(cid)]
 			response = response + "<button><a href='{0}/{1}' target='_blank'>{2}</a></button>\n\n".format(url_card, cid, client_name)
 	else:
 		response = "По данному запросу ничего не найдено"
